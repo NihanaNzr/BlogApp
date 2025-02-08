3dfd219e333ecfc6923708d1bd7e2f8051a10886
Nihana Nizar <nihananizr@gmail.com>
2025-02-08 19:49:26 +0530

    update post details

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post  # Ensure Post model is correctly imported
+from .models import Post  
+from ckeditor.widgets import CKEditorWidget
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-class PostForm(forms.ModelForm):  # Add this class
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())  # Enable CKEditor in forms
+
     class Meta:
         model = Post
-        fields = ['title', 'content']  # Include other fields as needed
+        fields = ['title', 'content']
